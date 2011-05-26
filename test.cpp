f6bf08992fba5d940470b7245ae7a9f91d69b3b5
Shuaib <shuaib@bytehood.com>
2011-05-26 22:15:44 +0500

    Adds solution to test problem

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    while(cin>>n)
+    {
+        if(n==42)
+            return 0;
+        cout<<n<<endl;
+    }
+    return 0;
+}
